eb591998d916e9d6300d0a5ebfbaea517f17b0e2
Chris Patterson <cpatterson@microsoft.com>
2022-10-10 13:19:08 -0400

    sync

--- lpt/graph.py
+++ lpt/graph.py
@@ -1,70 +1,116 @@
+import dataclasses
 import logging
 import subprocess
 import sys
-from typing import Set, Tuple
+from typing import Dict, Optional, Set, Tuple
 
 logger = logging.getLogger("lpt.graph")
 
 
-def get_afters(service: str) -> Set[str]:
-    if service.startswith("-."):
-        return set()
+@dataclasses.dataclass
+class Service:
+    name: str
+    afters: Set[str]
+    time_to_activate: Optional[float]
+    active_enter_timestamp_monotonic: Optional[float]
+    inactive_exit_timestamp_monotonic: Optional[float]
 
-    try:
-        proc = subprocess.run(
-            ["systemctl", "show", "-P", "After", service],
-            check=True,
-            capture_output=True,
-            text=True,
+    def is_valid(self) -> bool:
+        return bool(
+            self.inactive_exit_timestamp_monotonic
+            or self.active_enter_timestamp_monotonic
         )
-        afters = set(proc.stdout.strip().split(" "))
+
+    @classmethod
+    def query(cls, service_name: str) -> "Service":
+        afters: Set[str] = set()
+        active_enter_timestamp_monotonic: Optional[float] = None
+        inactive_exit_timestamp_monotonic: Optional[float] = None
+        time_to_activate: float = 0.0
+
         try:
-            afters.remove("")
-        except KeyError:
-            pass
-        return afters
-    except subprocess.CalledProcessError as error:
-        print(f"failed to read service list for: {service} {error}", file=sys.stderr)
-        return set()
+            proc = subprocess.run(
+                ["systemctl", "show", "--", service_name],
+                check=True,
+                capture_output=True,
+                text=True,
+            )
+        except subprocess.CalledProcessError as error:
+            print(f"failed to show service={service_name} {error}", file=sys.stderr)
+            raise
 
+        lines = proc.stdout.strip().splitlines()
+        for line in lines:
+            line = line.strip()
 
-def verify_usage(service: str) -> bool:
-    if service.startswith("-."):
-        return True
+            field = "After="
+            if line.startswith(field):
+                field_len = len(field)
+                line = line[field_len:]
+                afters = set(line.split())
 
-    try:
-        subprocess.run(
-            ["systemctl", "status", service], check=True, capture_output=True, text=True
+            field = "ActiveEnterTimestampMonotonic="
+            if line.startswith(field):
+                field_len = len(field)
+                line = line[field_len:]
+                active_enter_timestamp_monotonic = float(line) / 1000000
+
+            field = "InactiveExitTimestampMonotonic="
+            if line.startswith(field):
+                field_len = len(field)
+                line = line[field_len:]
+                inactive_exit_timestamp_monotonic = float(line) / 1000000
+
+        if active_enter_timestamp_monotonic and inactive_exit_timestamp_monotonic:
+            time_to_activate = (
+                active_enter_timestamp_monotonic - inactive_exit_timestamp_monotonic
+            )
+
+        return cls(
+            name=service_name,
+            afters=afters,
+            active_enter_timestamp_monotonic=active_enter_timestamp_monotonic,
+            inactive_exit_timestamp_monotonic=inactive_exit_timestamp_monotonic,
+            time_to_activate=time_to_activate,
         )
-    except subprocess.CalledProcessError as error:
-        print(f"failed to verify service list for: {service} {error}", file=sys.stderr)
-        return False
-    return True
 
 
-def walk_dependencies(service: str) -> Set[Tuple[str, str]]:
+def walk_dependencies(
+    service_name: str, services: Dict[str, Service]
+) -> Set[Tuple[Service, Service]]:
     deps = set()
     seen = set()
 
-    def _walk_dependencies(service: str) -> None:
-        if service in seen:
+    def _walk_dependencies(service_name: str) -> None:
+        if service_name in seen:
             return
-        seen.add(service)
+        seen.add(service_name)
 
-        for dep in get_afters(service):
-            if not verify_usage(dep):
-                continue
-            deps.add((service, dep))
-            _walk_dependencies(dep)
+        service = Service.query(service_name)
+        if service is None:
+            return
 
-    _walk_dependencies(service)
+        services[service_name] = service
+        for dep_name in service.afters:
+            if dep_name in services:
+                service_dep = services[dep_name]
+            else:
+                service_dep = Service.query(dep_name)
+
+            deps.add((service, service_dep))
+            _walk_dependencies(dep_name)
+
+    _walk_dependencies(service_name)
     return deps
 
 
-def generate_dependency_digraph(service: str) -> str:
-    deps = walk_dependencies(service)
-    lines = [f'digraph "{service}" {{', "  rankdir=LR;"]
-    for s1, s2 in sorted(deps):
-        lines.append(f'  "{s1}"->"{s2}" [color="green"];')
-    lines.append("}")
+def generate_dependency_digraph(service_name: str) -> str:
+    services: Dict[str, Service] = {}
+    deps = walk_dependencies(service_name, services)
+
+    edges = set()
+    for s1, s2 in sorted(deps, key=lambda x: x[0].name):
+        edges.add(f'  "{s1}"->"{s2}" [color="green"];')
+
+    lines = [f'digraph "{service_name}" {{', "  rankdir=LR;", *sorted(edges), "}"]
     return "\n".join(lines)
