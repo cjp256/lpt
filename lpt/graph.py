2d78784c2b1aaabbfab06ec30b833ebf08b1d9cf
Chris Patterson <cpatterson@microsoft.com>
2022-10-12 11:09:54 -0400

    sync

--- lpt/graph.py
+++ lpt/graph.py
@@ -215,18 +215,15 @@ def generate_dependency_digraph(
     systemd = Systemd.query()
 
     def _label_svc(service: Service) -> str:
-        label = service.name
+        label = f"service.name ["
         if service.time_to_activate:
-            label += f" +{service.time_to_activate:.02f}s"
+            label += f"+{service.time_to_activate:.02f}s "
 
         service_start = (
             service.active_enter_timestamp_monotonic
             - systemd.userspace_timestamp_monotonic
         )
-        label += f" @{service_start:.02f}s"
-
-        print(service, systemd)
-
+        label += f" @{service_start:.02f}s]"
         return label
 
     edges = set()
