8168c6726922a653ee1e7c3bd1784c2ce31079df
Chris Patterson <cpatterson@microsoft.com>
2022-10-19 13:24:26 -0400

    sync

--- lpt/graph.py
+++ lpt/graph.py
@@ -146,7 +146,7 @@ class ServiceGraph:
             "    style=filled;",
             "    color=lightgrey;",
             "    node [style=filled,color=grey];",
-            "    label=systemd-units",
+            '    label="systemd-units"',
             *edges,
             "  }",
         ]
@@ -178,7 +178,7 @@ class ServiceGraph:
                 "    style=filled;",
                 "    color=lightgrey;",
                 "    node [style=filled,color=pink];",
-                f"   label={label}",
+                f'   label="{label}"',
                 *edges,
                 "  }",
             ]
