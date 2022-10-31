a5f2dc2dd88399dac0218ca9884c9b751e1b549d
Chris Patterson <cpatterson@microsoft.com>
2022-10-31 13:25:49 -0400

    sync

---
+++ tests/integration/conftest.py
@@ -0,0 +1,11 @@
+import logging
+import os
+
+# content of conftest.py
+def pytest_configure(config):
+    worker_id = os.environ.get("PYTEST_XDIST_WORKER")
+    if worker_id is not None:
+        logging.basicConfig(
+            filename=f"/tmp/lpt-tests/tests_{worker_id}.log",
+            level=logging.DEBUG,
+        )
